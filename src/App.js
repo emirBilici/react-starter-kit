0749563346aa1ce697309e8aad8a3f95729335d8
Emir Furkan Bilici <103577802+emirBilici@users.noreply.github.com>
2022-07-04 00:33:08 +0300

    env files added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      {process.env.NODE_ENV === "development" && (
+          <h3>Hello World from React!</h3>
+      )}
+      <p>{process.env.REACT_APP_API_URL}</p>
+    </div>
   );
 }
 
