e193a8a1d30e1b3dda57fee29972c8f8462659c4
Emir Furkan Bilici <103577802+emirBilici@users.noreply.github.com>
2022-07-07 00:17:26 +0300

    last update :)

---
+++ src/Style.js
@@ -0,0 +1,25 @@
+import styles from './App.module.css';
+import Test from './Test';
+import { Title } from  './Components';
+import Bootsrap from './Bootsrap';
+
+// Tailwind
+//import './tailwind.css';
+import './style.scss';
+import Tailwind from './tailwind';
+
+function Styles() {
+    return(
+        <div className={styles.App}>
+            {process.env.NODE_ENV === "development" && (
+                <Title theme="dark">Hello World from React!</Title>
+            )}
+            <p>{process.env.REACT_APP_API_URL}</p>
+            <Test />
+            <Bootsrap />
+            <Tailwind />
+        </div>
+    );
+}
+
+export default Styles;
